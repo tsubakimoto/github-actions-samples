b364eb75fb1079fa267dbdd2926433aec20dfece
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2022-01-21 14:17:03 +0900

    Update main.py

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "FastAPI World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
