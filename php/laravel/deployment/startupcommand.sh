ed72b7de1dce8064e24feb33cbf003bab5bcc572
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2022-01-29 21:30:24 +0900

    Update startupcommand.sh

--- php/laravel/deployment/startupcommand.sh
+++ php/laravel/deployment/startupcommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replacing nginx configuration for serving PHP 8.0-based applications"
-cp /home/site/repository/deployment/nginx.conf /etc/nginx/sites-available/default
+cp /home/site/wwwroot/deployment/nginx.conf /etc/nginx/sites-available/default
 
 echo "Reloading nginx to apply new configuration"
 service nginx reload
