3a6fa7d55a126a6c828c0802403401bfe8e55507
Yuta Matsumura <tkmbmbr@gmail.com>
2022-01-29 23:39:52 +0900

    Delete Laravel app

--- php/laravel/deployment/startupcommand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replacing nginx configuration for serving PHP 8.0-based applications"
-cp /home/site/wwwroot/deployment/nginx.conf /etc/nginx/sites-available/default
-
-echo "Reloading nginx to apply new configuration"
-service nginx reload
