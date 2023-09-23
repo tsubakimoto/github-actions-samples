e7da4a62d1751fe495eecb18f99786a365733174
Yuta Matsumura <tkmbmbr@gmail.com>
2023-09-23 12:08:54 +0900

    not use url rewrite

--- php/cakephp5/config/app.php
+++ php/cakephp5/config/app.php
@@ -55,7 +55,7 @@ return [
         'dir' => 'src',
         'webroot' => 'webroot',
         'wwwRoot' => WWW_ROOT,
-        //'baseUrl' => env('SCRIPT_NAME'),
+        'baseUrl' => env('SCRIPT_NAME'),
         'fullBaseUrl' => false,
         'imageBaseUrl' => 'img/',
         'cssBaseUrl' => 'css/',
