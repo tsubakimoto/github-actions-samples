e1d221696a425b7f20a0010addb6a2b67c6ea749
Yuta Matsumura <tkmbmbr@gmail.com>
2024-05-05 14:13:09 +0900

    add nuget workflow

---
+++ dotnet/netstandard2.0/Standard20Library/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Standard20Library;
+
+public interface ICalculator
+{
+    public int Add(int a, int b);
+    public int Subtract(int a, int b);
+    public int Multiply(int a, int b);
+    public int Divide(int a, int b);
+}
+
+public class Calculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
