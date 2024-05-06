1200ef60d74c5ec5b7daf21232baaa89112b6ba2
Yuta Matsumura <tsubakimoto@users.noreply.github.com>
2024-05-07 00:12:28 +0900

    update django settings.py

--- python/django/django_hello/settings.py
+++ python/django/django_hello/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# https://github.com/MicrosoftDocs/azure-docs/issues/96982
+# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['CUSTOM_HOSTNAME']] if 'CUSTOM_HOSTNAME' in os.environ else []
 
 
 # Application definition
