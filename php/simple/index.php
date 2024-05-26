2983af176458fd7733720d5010e72b4a54e6a5e1
Yuta Matsumura <tkmbmbr@gmail.com>
2024-05-26 05:54:50 +0000

    update simple php app

--- php/simple/index.php
+++ php/simple/index.php
@@ -3,4 +3,6 @@ require 'vendor/autoload.php';
 
 use Carbon\Carbon;
 
-printf("Hello PHP World! Now: %s", Carbon::now());
+printf("Right now is %s", Carbon::now()->toDateTimeString());
+printf(nl2br("\n"));
+printf("Right now in Tokyo is %s", Carbon::now('Asia/Tokyo'));
