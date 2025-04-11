f88c4389d25eab0e3ecfdc7ffb2be79671dd7afd
Yuta Matsumura <tkmbmbr@gmail.com>
2025-04-11 09:27:55 +0900

    .NET 10 アプリケーションの作成

---
+++ dotnet/net10.0/razorpageapp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpageapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
